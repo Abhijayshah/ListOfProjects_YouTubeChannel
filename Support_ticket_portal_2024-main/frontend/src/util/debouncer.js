b388ada7c3b37858e11d74f4a50d23fdf5350c06
Abhijayshah <abhijayshah74@gmail.com>
2025-04-27 23:53:31 +0530

    all

--- Support_ticket_portal_2024-main/frontend/src/util/debouncer.js
+++
@@ -1,8 +0,0 @@
-export default function debouncer(cb, delay) {
-  let timerId = null;
-
-  return function (...args) {
-    if (timerId) clearTimeout(timerId);
-    timerId = setTimeout(() => cb(...args), delay);
-  };
-}
